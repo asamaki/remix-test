1e66bc8ce4e9fc236b3dee2163789f1f959cef04
asamaki <masahiro.ikai@gmail.com>
2024-06-27 01:03:09 +0900

    アイコン追加

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 import stylesheet from "~/tailwind.css?url";
 import PrelineScript from "~/components/preline.client";
+import HeaderIcon from "/public/icon-ghost-tools.png"
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
@@ -26,7 +27,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-primary-950 text-sm py-3 sm:py-0 ">
             <nav className="relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
               <div className="flex items-center justify-between sm:my-4 sm:ps-4">
-                <a className="flex-none text-xl font-semibold text-white" href="/" aria-label="Brand">ゴーストツール</a>
+                
+                
+                <a className="flex-none text-xl font-semibold text-white" href="/" aria-label="Brand"><img className="inline-block size-[34px] mr-2" src={HeaderIcon} alt="Image Description"/>ゴーストツール</a>
               </div>
             </nav>
           </header>
